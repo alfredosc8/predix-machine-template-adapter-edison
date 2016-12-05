c34e9610088fdf416a6051eb871c537c8f59fdc2
Predix Robot <predix-robot@ge.com>
2016-12-05 15:20:16 -0800

    First commit

---
+++ scripts/quickstart.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+CURRENT_DIR=$(pwd)
+curl -s https://github.com/raw/PredixDev/predix-machine-templates/master/scripts/buildAndDeploy.sh > scripts/buildAndDeploy.sh
+chmod 775 scripts/buildAndDeploy.sh
+./scripts/buildAndDeploy.sh $*
+
+rm -rf ./scripts/buildAndDeploy.sh
+
+PREDIX_SERVICES_SUMMARY_FILE="$CURRENT_DIR/predix-scripts/bash/log/predix-services-summary.txt"
+
+echo "" >> "$PREDIX_SERVICES_SUMMARY_FILE"
+echo "Edge Device Specific Configuration" >> "$PREDIX_SERVICES_SUMMARY_FILE"
+echo "What did we do:"  >> "$PREDIX_SERVICES_SUMMARY_FILE"
+echo "We setup some configuration files in the Predix Machine container to read from a DataNode for our sensors"  >> "$PREDIX_SERVICES_SUMMARY_FILE"
+echo "We installed some Intel API jar files that represent the Intel mraa and upm API" >> "$PREDIX_SERVICES_SUMMARY_FILE"
+echo "We built and deployed the Machine Adapter bundle which reads from the Intel API" >> "$PREDIX_SERVICES_SUMMARY_FILE"
+echo "" >> "$PREDIX_SERVICES_SUMMARY_FILE"
+
+echo "Summary File available at $PREDIX_SERVICES_SUMMARY_FILE" "#"
+cat $PREDIX_SERVICES_SUMMARY_FILE
+echo "Success - the script is complete" "#"
+
